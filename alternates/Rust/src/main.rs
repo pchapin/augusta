4fab3c0b595300479ca712900a09371b5a432a31
Peter Chapin <2094217+pchapin@users.noreply.github.com>
2025-02-03 19:01:13 -0500

    Created skeletal files for literal decoding in AGCrust.

--- alternates/Rust/src/main.rs
+++ alternates/Rust/src/main.rs
@@ -1,3 +1,5 @@
+mod scanner;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ alternates/Rust/src/scanner/literals.rs
@@ -0,0 +1,8 @@
+
+use num_bigint::BigInt;
+use num_traits::{Zero, One};
+
+pub fn decode_integer_literal(text: &str) -> BigInt {
+    // For now.
+    BigInt::zero()
+}
