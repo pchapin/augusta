70354c7d9930f49ae01385fd3da2dbe3cf4a5f77
Peter Chapin <2094217+pchapin@users.noreply.github.com>
2025-03-12 14:54:27 -0400

    Main now runs the compiler pipeline. Added Decline for command line parsing.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,18 +8,22 @@ object Dependencies {
   lazy val scalaGraphCoreVersion = "2.0.2"
   lazy val catsCoreVersion = "2.12.0"
   lazy val catsEffectVersion = "3.5.6"
+  lazy val declineVersion = "2.4.1"
+  lazy val declineEffectVersion = "2.4.1"
   lazy val kiamaVersion = "2.5.1"
 
   // Libraries
-  val scalactic      = "org.scalactic"   %% "scalactic"   % scalaTestVersion
-  val scalaTest      = "org.scalatest"   %% "scalatest"   % scalaTestVersion
-  val scalaGraphCore = "org.scala-graph" %% "graph-core"  % scalaGraphCoreVersion
-  val catsCore       = "org.typelevel"   %% "cats-core"   % catsCoreVersion
-  val catsEffect     = "org.typelevel"   %% "cats-effect" % catsEffectVersion
+  val scalactic      = "org.scalactic"   %% "scalactic"      % scalaTestVersion
+  val scalaTest      = "org.scalatest"   %% "scalatest"      % scalaTestVersion
+  val scalaGraphCore = "org.scala-graph" %% "graph-core"     % scalaGraphCoreVersion
+  val catsCore       = "org.typelevel"   %% "cats-core"      % catsCoreVersion
+  val catsEffect     = "org.typelevel"   %% "cats-effect"    % catsEffectVersion
+  val decline        = "com.monovore"    %% "decline"        % declineVersion
+  val declineEffect  = "com.monovore"    %% "decline-effect" % declineEffectVersion
   val kiama          = "org.bitbucket.inkytonik.kiama" %% "kiama" % kiamaVersion
 
   // Projects
 
   val augustaDeps: Seq[ModuleID] =
-      Seq(kiama, catsEffect, catsCore, scalaGraphCore, scalactic, scalaTest % Test)
+      Seq(kiama, declineEffect, decline, catsEffect, catsCore, scalaGraphCore, scalactic, scalaTest % Test)
 }
--- src/main/scala/org/kelseymountain/agc/Main.scala
+++ src/main/scala/org/kelseymountain/agc/Main.scala
@@ -1,18 +1,61 @@
 package org.kelseymountain.agc
 
-import cats.effect._
-import org.antlr.v4.runtime._
-import org.antlr.v4.runtime.tree._
+import cats.effect.{ExitCode, IO}
+import cats.syntax.all.*
+import com.monovore.decline.*
+import com.monovore.decline.effect.*
+import org.antlr.v4.runtime.*
+import org.antlr.v4.runtime.tree.*
+
+private def processAugustaFile(filePath: String): IO[Unit] = {
+  for {
+    input <- IO.blocking(CharStreams.fromFileName(filePath))
+    lexer  = new AugustaLexer(input)
+    tokens = new CommonTokenStream(lexer)
+    parser = new AugustaParser(tokens)
+    tree = parser.compilation_unit()
+    _ <- IO {
+      val myConsoleReporter = new BasicConsoleReporter
+      val mySymbolTable = new SymbolTableTree(preDefinedSymbols)
+      val mySymbolTableBuilder = new SymbolTableBuilder(myConsoleReporter, mySymbolTable)
+      mySymbolTableBuilder.visit(tree)
+      val mySemanticAnalyzer = new SemanticAnalyzer(myConsoleReporter, mySymbolTable)
+      mySemanticAnalyzer.visit(tree)
+      val myCGenerator = new CGenerator(myConsoleReporter, mySymbolTable)
+      myCGenerator.visit(tree)
+    }
+  } yield ()
+}
 
 /**
   * The main object of the Augusta.
   */
-object Main extends IOApp {
+object Main extends CommandIOApp(
+  name    = "agc",
+  header  = "AGC Compiler - A compiler for the Augusta language",
+  version = "0.1.0") {
 
-  // Just the sample from the Cats Effect README for now...
-  def run(args: List[String]): IO[ExitCode] =
-    if (args.headOption.map(_ == "--do-it").getOrElse(false))
-      IO.println("I did it!").as(ExitCode.Success)
-    else
-      IO.println("I didn't do it.").as(ExitCode(-1))
+  // Define an option for setting the optimization level.
+  val optimizationOpt: Opts[Int] = Opts
+    .option[Int]("optimize", short = "O", help = "Set optimization level (0-3)")
+    .withDefault(0)
+
+  // Define an option for specifying the output file.
+  val outputOpt: Opts[String] = Opts
+    .option[String]("output", short = "o", help = "Output file name")
+    .withDefault("a.out")
+
+  // Define a positional argument for the input file.
+  val inputFile: Opts[String] = Opts
+    .argument[String]("input file")
+
+  // Combine all options into a single command.
+  override def main: Opts[IO[ExitCode]] = (optimizationOpt, outputOpt, inputFile).mapN {
+    (optLevel, outputFile, fileName) =>
+      processAugustaFile(fileName)
+        .as(ExitCode.Success)
+        .handleErrorWith { err =>
+          IO.println(s"Error: ${err.getMessage}").as(ExitCode.Error)
+        }
+    }
 }
