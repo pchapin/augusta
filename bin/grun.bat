e508f9f1941ff5e266e348cb87a5ccedd116356c
Peter Chapin <2094217+pchapin@users.noreply.github.com>
2025-03-19 23:00:45 -0400

    The grun batch file works again.

--- bin/grun.bat
+++ bin/grun.bat
@@ -1,14 +1,16 @@
 @echo off
 REM
 REM This is the testing tool described in "Definitive ANTRL 4."
+REM Usage: From the root of the repository run bin\grun.bat as follows:
 REM
+REM bin\grun.bat <start-rule> [-tokens|-tree|-gui] <path-to-input-file>
 
-REM Set up the class path to grab the jar files from the Ivy cache.
-set CPATH=%HOME%\.ivy2\cache\org.antlr\antlr4\jars\antlr4-4.13.2.jar
-set CPATH=%CPATH%;%HOME%\.ivy2\cache\org.antlr\antlr4-runtime\jars\antlr4-runtime-4.13.2.jar
-set CPATH=%CPATH%;%HOME%\.ivy2\cache\org.abego.treelayout\org.abego.treelayout.core\bundles\org.abego.treelayout.core-1.0.3.jar
+REM Set up the class path to grab the jar files from the Coursier cache.
+set COURSIER=%USERPROFILE%\AppData\Local\Coursier\cache\v1\https\repo1.maven.org\maven2
+set CPATH=%COURSIER%\org\antlr\antlr4\4.13.2\antlr4-4.13.2.jar
+set CPATH=%CPATH%;%COURSIER%\org\antlr\antlr4-runtime\4.13.2\antlr4-runtime-4.13.2.jar
+set CPATH=%CPATH%;%COURSIER%\org\abego\treelayout\org.abego.treelayout.core\1.0.3\org.abego.treelayout.core-1.0.3.jar
+set CPATH=%CPATH%;target\scala-3.3.4\classes
 
-REM Execute the tool on the Ada grammar. Note that the test data is at ..\..\..\testData
-cd target\scala-3.3.4\classes
-java -cp .;%CPATH% org.antlr.v4.gui.TestRig org.pchapin.augusta.Ada %*
-cd ..\..\..
+REM Execute the tool on the Augusta grammar.
+java -cp .;%CPATH% org.antlr.v4.gui.TestRig org.kelseymountain.agc.Augusta %*
